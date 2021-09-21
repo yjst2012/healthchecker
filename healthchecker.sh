7ae842d7e83b074c80042e4c174922e496a553e7
Jeff Y <17779519+yjst2012@users.noreply.github.com>
2021-09-21 16:42:30 +1200

    initial commit

---
+++ healthchecker.sh
@@ -0,0 +1,23 @@
+#!bin/bash  
+
+
+# all hosts listed here, separated by space
+ALL_HOSTS=(192.168.9.1 192.168.9.2 192.168.9.3 192.168.9.4 192.168.9.5)
+
+for host in ${ALL_HOSTS[*]}
+do
+{
+    start_time=$(date +'%s')
+    ssh $host "hostname" &>/dev/null
+    sleep 2
+    stop_time=$(date +'%s')
+    time_consuming=$((stop_time-start_time))
+    echo "$host: $time_consuming" >>hostname.txt
+}&
+done
+
+wait
+
+host=$(sort -n -k 2 hostname.txt | head -1 | awk -F':' '{print $1}')
+
+ssh $host "top -b -n 1"
